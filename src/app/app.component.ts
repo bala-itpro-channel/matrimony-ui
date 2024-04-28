ae13eac51ec07226c3983b0b11ea0528f596b36f
bala-itpro-channel <bala.itpro.channel@gmail.com>
2024-04-28 01:22:03 -0400

    Matrimony web app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'matrimony-app';
+  @ViewChild('myTopnav', {read: ElementRef}) myTopnav!: ElementRef;
+
+  myFunction() {
+    const x: HTMLElement | null = this.myTopnav.nativeElement;
+    if (x!.className === 'topnav') {
+      x!.className += ' responsive';
+    } else {
+      x!.className = 'topnav';
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { HelpComponent } from './components/help/help.component';
+import { SearchComponent } from './components/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent, pathMatch: 'full'
+    },
+    {
+        path: 'registration', component: RegistrationComponent
+    },
+    {
+        path: 'signin', component: SigninComponent
+    },
+    {
+        path: 'help', component: HelpComponent
+    },
+    {
+        path: 'search', component: SearchComponent
+    },
+];
